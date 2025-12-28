73fae700568e0acf86be226d0de186dfb0eb8de2
yamaneko05 <o.yamaneko0331@gmail.com>
2025-12-28 13:48:38 +0900

    add: ロゴとアイコン

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -2,14 +2,22 @@ import Link from "next/link";
 import { Settings } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { GroupSwitcher } from "./group-switcher";
+import Image from "next/image";
 
 export function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-backdrop-filter:bg-background/60">
       <div className="flex h-14 items-center justify-between px-4">
         <div className="flex items-center gap-4">
-          <Link href={"/"}>
-            <h1 className="text-2xl font-semibold">Stockee</h1>
+          <Link href={"/"} className="flex items-center">
+            <Image
+              src="/icon.png"
+              alt="icon"
+              width={36}
+              height={36}
+              unoptimized
+            />
+            <h1 className="text-xl font-bold font-logo">Stockee</h1>
           </Link>
           <GroupSwitcher />
         </div>
